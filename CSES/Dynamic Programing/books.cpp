af094e65ab04df6fc7cf01e050972ee7630d04e1
Mateo <83977126+quirosaspron@users.noreply.github.com>
2026-08-28 20:19:09 -0600

    added an explanation of the problem

--- CSES/Dynamic Programing/books.cpp
+++ CSES/Dynamic Programing/books.cpp
@@ -1,5 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+You are in a book shop which sells n different books. You know the price and number of pages of each book.
+You have decided that the total price of your purchases will be at most x. What is the maximum number of pages you can buy? You can buy each book at most once.
+Input
+The first input line contains two integers n and x: the number of books and the maximum total price.
+The next line contains n integers h_1,h_2,\ldots,h_n: the price of each book.
+The last line contains n integers s_1,s_2,\ldots,s_n: the number of pages of each book.
+Output
+Print one integer: the maximum number of page
+// Problem: Books - CSES 
+/*
+* n books, each with price and number of pages. You can spend at most x
+* What is the maximum pages you can buy? 
+* The idea is to build a dp table from the bottom up. 
+* dp[book][budget] <- Max ammount of pages you can buy having seen up to that book with that ammount of budget
+* To fill the table:
+* you ask: Can I buy it? if you can't the best you can do is the last solution dp[book-1][x]
+* If you can, you ask: Should you buy it? 
+* You should buy it if the ammount of pages and the best solution with your new budget is greater than that of dp[book-1][x]
+* You go through each book and increase the budget up to x, after that you go on to the next book. With each book you see you have more information. 
+* at the end you just need to print dp[n][x]
+*/
+
 int main(){
     int n, x, i;
     cin >> n;
